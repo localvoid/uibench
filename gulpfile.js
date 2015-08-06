57a5121eef78fd233d8d3b58ac6cada9109aeac2
Boris Kaul <localvoid@gmail.com>
2015-08-06 13:02:08 +0600

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,8 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
-var webpackConfig = require('./webpack.config.js');
 var ghPages = require('gulp-gh-pages');
+var path = require('path');
 
 gulp.task('html', function() {
   gulp.src('web/index.html')
@@ -12,40 +12,66 @@ gulp.task('html', function() {
 });
 
 gulp.task('build', function(callback) {
-  var myConfig = Object.create(webpackConfig);
-  myConfig.entry = [
-    './web/js/main.jsx'
-  ];
-  myConfig.plugins = myConfig.plugins || [];
-  myConfig.plugins = myConfig.plugins.concat(
-      new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify('production')
-        }
-      }),
+  var cfg = {
+    entry: ['./web/js/main.jsx'],
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: 'bundle.js'
+    },
+    resolve: {
+      extensions: ['', '.js', '.jsx']
+    },
+    module: {
+      loaders: [{
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loaders: ['babel']
+      }]
+    },
+    plugins: [
+      new webpack.NoErrorsPlugin(),
+      new webpack.DefinePlugin({'process.env': {NODE_ENV: 'production'}}),
       new webpack.optimize.DedupePlugin(),
       new webpack.optimize.UglifyJsPlugin()
-  );
+    ]
+  };
 
-  // run webpack
-  webpack(myConfig, function(err, stats) {
-    if (err) {
-      throw new gutil.PluginError('build', err);
-    }
-    gutil.log('[build]', stats.toString({
-      colors: true
-    }));
+  webpack(cfg, function(err, stats) {
+    if (err) throw new gutil.PluginError('build', err);
+    gutil.log('[build]', stats.toString({colors: true}));
     callback();
   });
 });
 
 
-gulp.task('serve', function(callback) {
-  var myConfig = Object.create(webpackConfig);
-  myConfig.devtool = 'eval';
-  myConfig.debug = true;
+gulp.task('serve', ['html'], function(callback) {
+  var cfg = {
+    entry: [
+      'webpack-dev-server/client?http://0.0.0.0:8080',
+      './web/js/main.jsx'
+    ],
+    devtool: 'eval',
+    debug: true,
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: 'bundle.js'
+    },
+    resolve: {
+      extensions: ['', '.js', '.jsx']
+    },
+    module: {
+      loaders: [{
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loaders: ['babel']
+      }]
+    },
+    plugins: [
+      new webpack.NoErrorsPlugin()
+    ]
+  };
 
-  new WebpackDevServer(webpack(myConfig), {
+  new WebpackDevServer(webpack(cfg), {
     contentBase: './build',
     stats: {
       colors: true
