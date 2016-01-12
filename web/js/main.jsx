6088f288aa483d68d977968eb438dc7e7f0b4f02
Boris Kaul <localvoid@gmail.com>
2016-01-12 19:39:12 +0600

    Remove React (dev version) from the list

--- web/js/main.jsx
+++ web/js/main.jsx
@@ -324,11 +324,6 @@ document.addEventListener('DOMContentLoaded', function(e) {
         'benchmarkUrl': 'https://localvoid.github.io/uibench-react/'
       },
       {
-        'name': 'React (latest dev version)',
-        'url': 'https://facebook.github.io/react/',
-        'benchmarkUrl': 'https://localvoid.github.io/uibench-react-dev/'
-      },
-      {
         'name': 'Bobril',
         'url': 'https://github.com/Bobris/Bobril',
         'benchmarkUrl': 'https://bobris.github.io/uibench-bobril/'
